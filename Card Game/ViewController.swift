912a4f45deebce2909190d5da9ea98518c8686bb
Nursurtan <arapovnur23@gmail.com>
2021-09-01 22:53:44 +0700

    Created a card game like the dice app

--- Card Game/ViewController.swift
+++ Card Game/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cardImageViewOne: UIImageView!
+    
+    @IBOutlet weak var cardImageViewTwo: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let cardArray = [#imageLiteral(resourceName: "card2"),#imageLiteral(resourceName: "card3"),#imageLiteral(resourceName: "card4"),#imageLiteral(resourceName: "card5"),#imageLiteral(resourceName: "card6"),#imageLiteral(resourceName: "card7"),#imageLiteral(resourceName: "card8"),#imageLiteral(resourceName: "card9"),#imageLiteral(resourceName: "card10"),#imageLiteral(resourceName: "card11"),#imageLiteral(resourceName: "card12"),#imageLiteral(resourceName: "card13"),#imageLiteral(resourceName: "card14"),]
+        
+        cardImageViewOne.image = cardArray.randomElement()
+        
+        cardImageViewTwo.image = cardArray.randomElement()
+    }
+    
 }
 
