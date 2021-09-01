095032d6454ce86c16010b6a7ca3e2f1be242f77
Nursurtan <arapovnur23@gmail.com>
2021-09-01 20:57:00 +0700

    Initial Commit

---
+++ Card Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Card Game
+//
+//  Created by нурсултан арапов on 9/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
